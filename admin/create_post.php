2ed1fcff7120fcad5f1140729d7f4b613df56c68
leviGatimu <getmorelev@gmail.com>
2026-02-22 18:17:32 +0200

    Updates: 2026-02-22T16:17:29.947Z

--- admin/create_post.php
+++ admin/create_post.php
@@ -27,10 +27,10 @@ require '../includes/db_connect.php';
             <h1 class="text-xl font-bold">Write New Post</h1>
         </div>
         <div>
-            <button onclick="document.getElementById('postForm').submit();" class="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-6 rounded-lg shadow-sm transition-all flex items-center gap-2">
-                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"></path></svg>
-                Save Post
-            </button>
+            <button onclick="savePost()" class="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-6 rounded-lg shadow-sm transition-all flex items-center gap-2">
+    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"></path></svg>
+    Save Post
+</button>
         </div>
     </header>
 
