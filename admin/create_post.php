fdfbf6573b58dbc8882b9e933519312a96d111fc
leviGatimu <getmorelev@gmail.com>
2026-02-22 18:11:25 +0200

    Updates: 2026-02-22T16:11:23.608Z

--- admin/create_post.php
+++ admin/create_post.php
@@ -8,7 +8,7 @@ require '../includes/db_connect.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Write New Post - Levi's Admin</title>
+    <title>Write New Post - Levi's Admin.</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
     
